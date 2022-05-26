d998f7356c4ae8aef7a1a609c95cd9803a839b40
andydangVN <dangminhnghia@gmail.com>
2022-05-26 16:50:41 +0700

    Body-parser npm parse body html in post method

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 1. npm install express
 3. npm init
-4. npm install nodemon
+4. npm install nodemon - auto refresh server with any change
+   npm install body-parser - lấy dữ liệu respond truyền về từ method post.
+   https://www.npmjs.com/package/body-parser#bodyparserurlencodedoptions
 5. Hyper CLI: atom .
 6. Hyper CLI: touch index.html - tạo file
-7. Send file html vào request: ```res.sendFile(**__dirname** + "/index.html");```
+7. Send file html vào request: ```res.sendFile(__dirname + "/index.html");```
+  __dirname: relative path (path của file đang truy cập)
 8. Send element html: ```res.send("<h1>hello</h1>")```
--- calculator.js
+++ calculator.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 const port = (3000);
 
+// body-parser application
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(port, function(){
   console.log(`Example app listening on port ${port}`)
 });
 
+// route: / (home) is index.html
 app.get('/', function(req, res){
   res.sendFile(__dirname + "/index.html");
+});
+
+// get infor from form use method post
+app.post('/', function(req, res){
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+  res.send("Result: " + result);
+  console.log(req.body);
 });
